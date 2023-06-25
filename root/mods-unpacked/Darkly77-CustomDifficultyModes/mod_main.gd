bb6df7644c529598176d073f09d3111caa760f30
Chris Bloomfield <43499897+ithinkandicode@users.noreply.github.com>
2023-06-26 00:00:00 +0100

    2.0.1 - Update to ModLoader v6

--- root/mods-unpacked/Darkly77-CustomDifficultyModes/mod_main.gd
+++ root/mods-unpacked/Darkly77-CustomDifficultyModes/mod_main.gd
@@ -8,19 +8,21 @@ var ext_dir = ""
 
 
 func _init(modLoader = ModLoader):
-	ModLoaderUtils.log_info("Init", DIFFTEST_LOG)
-	dir = modLoader.UNPACKED_DIR + MOD_DIR
+	return
+	ModLoaderLog.info("Init", DIFFTEST_LOG)
+	dir = ModLoaderMod.get_unpacked_dir() + MOD_DIR
 	# ext_dir = dir + "extensions/"
 
 	# Add translations
-	modLoader.add_translation_from_resource(dir + "translations/mod_customdifficultymodes_text.en.translation")
+	ModLoaderMod.add_translation(dir + "translations/mod_customdifficultymodes_text.en.translation")
 
 
 func _ready():
+	return
 	var ContentLoader = get_node("/root/ModLoader/Darkly77-ContentLoader/ContentLoader")
 
 	# Add content
 	ContentLoader.load_data(dir + "content_data/items.tres", DIFFTEST_LOG)
 	ContentLoader.load_data(dir + "content_data/difficulties.tres", DIFFTEST_LOG)
 
-	ModLoaderUtils.log_info("Done", DIFFTEST_LOG)
+	ModLoaderLog.info("Done", DIFFTEST_LOG)
